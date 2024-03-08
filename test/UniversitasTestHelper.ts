df7707218bfa4c4a0e627b778eb416844a732d4c
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-08 17:34:56 +0800

    add universitas test helper

---
+++ test/UniversitasTestHelper.ts
@@ -0,0 +1,22 @@
+/* istanbul ignore file */
+import {prismaClient} from '../src/infrastructure/database/prisma';
+
+export const UniversitasTestHelper = {
+  async add(datas = [{
+    id: 1,
+    nama: 'tes',
+    alamat: 'tes st.',
+    keterangan: 'tes',
+    logo_url: 'tes.png',
+  }]) {
+    await prismaClient.universitas.createMany({data: datas});
+  },
+
+  async findById(id = 1) {
+    await prismaClient.universitas.findFirst({where: {id: id}});
+  },
+
+  async clean() {
+    await prismaClient.universitas.deleteMany();
+  },
+};
