64c84293e5583711f2c7b30360d2e85e583cc46d
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-15 18:33:05 +0800

    sekolah: add and init router

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import {errorMiddleware} from '../../Interface/http/middleware/error';
 import {universitasRouter} from '../../Interface/http/api/universitas/router';
+import {sekolahRouter} from '../../Interface/http/api/sekolah/router';
 
 export const initServer = () => {
   const app = express();
@@ -8,7 +10,8 @@ export const initServer = () => {
     res.status(200).json({message: 'Welcome to MyCampus API'});
   });
   app.use('/api/v1/images', express.static('public'));
-  app.use('/api/v1', universitasRouter());
+  app.use(bodyParser.json());
+  app.use('/api/v1', universitasRouter(), sekolahRouter());
   app.use(errorMiddleware);
 
   return app;
