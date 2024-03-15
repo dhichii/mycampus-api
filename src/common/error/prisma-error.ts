e1ec837b37677ba001c18bce537fc752515bb3a0
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-15 16:53:10 +0800

    change prisma unique error message from `must be unique` to `is already exist`

--- src/common/error/prisma-error.ts
+++ src/common/error/prisma-error.ts
@@ -4,7 +4,7 @@ import {ResponseError} from './response-error';
 export function isPrismaError(e: any) {
   if (e instanceof Prisma.PrismaClientKnownRequestError) {
     if (e.code === 'P2002') {
-      throw new ResponseError(400, `${e.meta?.target} must be unique`);
+      throw new ResponseError(400, `${e.meta?.target} is already exist`);
     }
   }
 }
