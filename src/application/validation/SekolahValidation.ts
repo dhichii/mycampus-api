cc0af2f6860148c9a69838af716df00fb8fbfd3c
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-15 18:22:46 +0800

    sekolah: change nama min validation to 10

--- src/application/validation/SekolahValidation.ts
+++ src/application/validation/SekolahValidation.ts
@@ -2,11 +2,11 @@ import {ZodType, z} from 'zod';
 
 export class SekolahValidation {
   static readonly ADD: ZodType = z.object({
-    nama: z.string().min(20).max(255),
+    nama: z.string().min(10).max(255),
   });
   static readonly EDIT_BY_ID: ZodType = z.object({
     id: z.string().uuid(),
-    nama: z.string().min(20).max(255),
+    nama: z.string().min(10).max(255),
   });
 
   static readonly DELETE_BY_ID: ZodType = z.object({
