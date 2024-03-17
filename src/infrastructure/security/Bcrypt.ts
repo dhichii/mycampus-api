ac28c958d567fb05c0385e4dee25c25d02ed3abb
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-17 23:18:00 +0800

    akun: add bcrypt helper to hash and compare password

---
+++ src/infrastructure/security/Bcrypt.ts
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+import {ResponseError} from '../../common/error/response-error';
+
+export class Bcrypt {
+  constructor(private readonly saltRound = 10) {}
+
+  async hash(password: string) {
+    return bcrypt.hash(password, this.saltRound);
+  }
+
+  async compare(password: string, hashedPassword: string) {
+    const res = await bcrypt.compare(password, hashedPassword);
+    if (!res) {
+      throw new ResponseError(404, 'email atau password salah');
+    }
+  }
+}
