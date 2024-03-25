7e2bfe987ca10b161d51852007f1156d74654d36
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-25 21:14:00 +0800

    authentication: set table collumn is_used default value to false

--- prisma/migrations/20240325131224_add_authentication_table/migration.sql
+++ prisma/migrations/20240325131224_add_authentication_table/migration.sql
@@ -2,7 +2,7 @@
 CREATE TABLE "authentication" (
     "token" VARCHAR(255) NOT NULL,
     "expires_at" TIMESTAMP(3) NOT NULL,
-    "is_used" BOOLEAN NOT NULL,
+    "is_used" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "authentication_pkey" PRIMARY KEY ("token")
 );
