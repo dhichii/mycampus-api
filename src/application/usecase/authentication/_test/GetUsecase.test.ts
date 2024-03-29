2b4b5a1056fbd8631f63e12f806d693ac1d328b1
Adhicitta Masran <adhicittamasran@gmail.com>
2024-03-29 17:50:44 +0800

    authentication: add usecase tests

---
+++ src/application/usecase/authentication/_test/GetUsecase.test.ts
@@ -0,0 +1,51 @@
+import {ResponseError} from '../../../../common/error/response-error';
+import {AuthenticationRepository}
+  from '../../../../domain/authentication/AuthenticationRepository';
+import {Jwt} from '../../../../infrastructure/security/Jwt';
+import {GetAuthenticationUsecase} from '../GetUsecase';
+
+describe('GetAuthenticationUsecase', () => {
+  it('should throw response error', async () => {
+    const mockAuthenticationRepo = {} as AuthenticationRepository;
+
+    mockAuthenticationRepo.get = jest.fn(() => Promise.resolve({
+      token: 'x',
+      expires_at: new Date(),
+      is_used: false,
+    }));
+    mockAuthenticationRepo.delete = jest.fn(() => Promise.resolve());
+
+    const usecase = new GetAuthenticationUsecase(mockAuthenticationRepo);
+
+    try {
+      await usecase.execute('x');
+
+      expect(mockAuthenticationRepo.get).toHaveBeenCalledWith('x');
+      expect(mockAuthenticationRepo.delete).toHaveBeenCalledWith('x');
+    } catch (e: any) {
+      console.log(e);
+      expect(e).toBeInstanceOf(ResponseError);
+      expect(e.message).toEqual('sesi kadaluarsa, silahkan login kembali');
+      expect(e.status).toEqual(401);
+    }
+  });
+
+  it('should orchestrating the get authentication correctly', async () => {
+    const jwt = new Jwt();
+    const refreshToken = await jwt.createRefreshToken('x');
+
+    const mockAuthenticationRepo = {} as AuthenticationRepository;
+
+    mockAuthenticationRepo.get = jest.fn(() => Promise.resolve({
+      token: refreshToken,
+      expires_at: new Date(),
+      is_used: false,
+    }));
+    mockAuthenticationRepo.delete = jest.fn(() => Promise.resolve());
+
+    const usecase = new GetAuthenticationUsecase(mockAuthenticationRepo);
+    const authentication = await usecase.execute(refreshToken);
+
+    expect(() => authentication).not.toThrow(ResponseError);
+  });
+});
