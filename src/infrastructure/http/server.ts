4d2df9d1b945995e7793c05090a0ece2d0c3d6cb
Adhicitta Masran <adhicittamasran@gmail.com>
2024-04-01 17:46:31 +0800

    authentication: init router

--- src/infrastructure/http/server.ts
+++ src/infrastructure/http/server.ts
@@ -5,6 +5,8 @@ import {errorMiddleware} from '../../Interface/http/middleware/error';
 import {universitasRouter} from '../../Interface/http/api/universitas/router';
 import {sekolahRouter} from '../../Interface/http/api/sekolah/router';
 import {adminRouter} from '../../Interface/http/api/admin/router';
+import {authenticationRouter}
+  from '../../Interface/http/api/authentication/router';
 
 export const initServer = () => {
   const app = express();
@@ -16,7 +18,13 @@ export const initServer = () => {
   app.use(bodyParser.json(), cookieParser());
 
   app.use('/api/v1/images', express.static('public'));
-  app.use('/api/v1', universitasRouter(), sekolahRouter(), adminRouter());
+  app.use(
+      '/api/v1',
+      universitasRouter(),
+      sekolahRouter(),
+      adminRouter(),
+      authenticationRouter(),
+  );
   app.use(errorMiddleware);
 
   return app;
