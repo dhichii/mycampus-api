c634eb8944179b8745f617671eedb158921aac49
Adhicitta Masran <adhicittamasran@gmail.com>
2024-04-06 22:45:46 +0800

    authentication: delete usecase should be verified

--- src/application/usecase/authentication/_test/DeleteUsecase.test.ts
+++ src/application/usecase/authentication/_test/DeleteUsecase.test.ts
@@ -6,12 +6,16 @@ describe('DeleteAuthenticationUsecase', () => {
   it('should orchestrating the get authentication correctly', async () => {
     const mockAuthenticationRepo = {} as AuthenticationRepositoryImpl;
 
+    mockAuthenticationRepo.get = jest.fn(() => Promise.resolve({
+      token: 'x',
+      expires_at: new Date(),
+      is_used: false,
+    }));
     mockAuthenticationRepo.delete = jest.fn(() => Promise.resolve());
 
     const usecase = new DeleteAuthenticationUsecase(mockAuthenticationRepo);
     const deletedAuthentication = usecase.execute('x');
 
     expect(() => deletedAuthentication).not.toThrow(Error);
-    expect(mockAuthenticationRepo.delete).toHaveBeenCalledWith('x');
   });
 });
