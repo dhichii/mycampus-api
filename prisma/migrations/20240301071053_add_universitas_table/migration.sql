dce21dfdc028f58750f4213b1aee0a6875128193
Adhicitta Masran <adhicittamasran@gmail.com>
2024-04-23 22:47:27 +0800

    build(universitas): add column jenis to the table

--- prisma/migrations/20240301071053_add_universitas_table/migration.sql
+++ prisma/migrations/20240301071053_add_universitas_table/migration.sql
@@ -1,7 +1,11 @@
+-- CreateEnum
+CREATE TYPE "Jenis_Universitas" AS ENUM ('NEGERI', 'SWASTA');
+
 -- CreateTable
 CREATE TABLE "universitas" (
     "id" SERIAL NOT NULL,
     "nama" VARCHAR(120) NOT NULL,
+    "jenis" "Jenis_Universitas" NOT NULL,
     "alamat" VARCHAR(255) NOT NULL,
     "keterangan" TEXT NOT NULL,
     "logo_url" VARCHAR(255) NOT NULL,
